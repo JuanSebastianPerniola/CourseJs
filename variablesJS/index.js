6b82bd125d9ff02ae9dc546a36df09000f1ee70b
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-01-14 21:32:57 +0100

    variables

---
+++ variablesJS/index.js
@@ -0,0 +1,13 @@
+//Arithmetic  operators on js =  operands(Nvalue, Nvalue, etc)
+//                              operators(+,-,*,/)
+//                              ex. 11 =  x + 5 
+
+let student  = 31;
+//student = student ** 5 ;
+//student = student / 1 ;
+//student = student * 1 ;
+//student = student + 1 ;
+//student = student - 1 ;
+//student  = student % 2;
+
+console.log(student);
