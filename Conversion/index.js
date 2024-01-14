4798f4db1c25e4929528b238962a7b80ed719424
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-01-14 21:30:58 +0100

    Conversiones javascript

---
+++ Conversion/index.js
@@ -0,0 +1,20 @@
+//typpe conversion = change a data tpye of a value for another 
+                //              (strings, numbers , booleans )
+                //datadate from user  = string  
+
+
+// let age = windwos.prompt("What you age");
+// age = Number(age);
+// age =+1;
+
+// console.log(age);
+
+let x  = "pizza";
+
+x = Number(x);  //turn a string into a int will not do much (NaN) but if we put x = "0" it will turn it into a number 
+
+let  y = "pizza"; 
+let y2 = ""; 
+
+y = Boolean(y); //Si queremos convertir y a un boolean si tiene algo, dara true
+y2 = Boolean(y2); //y si no tiene nada falso 
